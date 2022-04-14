c4b5ab01e41645048735119fb74e2b66ba7a60dc
Erin Swenson-Healey <l@s3r.me>
2022-04-13 21:06:56 -0700

    begin first assignment

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,15 +2,19 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n | n < 1 = []
+toDigits n | n < 10 = [n]
+toDigits n = toDigits (n `div` 10) ++ [n `mod` 10]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = reverse . toDigits
 
 -- #2
+-- it takes the second argument and the last item of the list and applies the function, then it takes the penultimate item from the end and the result, and so on. See scanr for intermediate results.
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther xs = snd $ foldr (\item (bit, acc) -> if not bit then (True, item * 2 : acc) else (False, item:acc)) (True, []) xs
 
 -- #3
 sumDigits :: [Integer] -> Integer
